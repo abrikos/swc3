63c5b1dedb8b4327e7c54109cc20bb000ef0f982
Artem Filippow <a.filippov@qtech.ru>
2025-06-03 23:21:51 +0900

    reload

--- reload.sh
+++ reload.sh
@@ -7,6 +7,9 @@ GIT=`git pull`
 if [[ $GIT =~ "Already" ]]; then
   echo $GIT
 else
+# Load nvm
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   nvm use 22.16.0
   npm i
   npm run build
