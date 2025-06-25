dcffd6570f1b9eb49bb9dc8940d2f86f1d84d01d
Artem Filippow <a.filippov@qtech.ru>
2025-06-25 18:09:05 +0900

    fix theme toggle control

--- components/ThemeSwitch.vue
+++ components/ThemeSwitch.vue
@@ -2,11 +2,15 @@
 import { useCookie } from '#app';
 const themeWidth = useCookie<string>('themeWidth');
 const drawerSide = useCookie<string>('drawerSide');
+onMounted(()=>{
+  if (!themeWidth.value) {themeWidth.value=''}
+  if (!drawerSide.value) {drawerSide.value=''}
+})
 </script>
 
 <template lang="pug">
   q-item
-    q-toggle(v-model="themeWidth" label="Узкий размер" false-value="" true-value="thin" )
+    q-toggle(v-model="themeWidth" label="Узкий размер" false-value="" true-value="right" )
   q-item
     q-toggle(v-model="drawerSide" label="Меню справа" false-value="" true-value="right" )
 </template>
