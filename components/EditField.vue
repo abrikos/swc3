192c671a222ebaa0a6c229278e0bd41ba067d5be
Artem Filippow <a.filippov@qtech.ru>
2025-07-18 15:10:45 +0900

    edit button

--- components/EditField.vue
+++ components/EditField.vue
@@ -4,6 +4,7 @@ const {update} = defineProps({update: Function})
 </script>
 
 <template lang="pug">
+    q-btn(icon="mdi-pencil" size="sm" )
     q-popup-edit.full-widthBAK(v-model="model" auto-save v-slot="scope")
       q-input(v-model="model" autofocus @keyup.enter.prevent="scope.set();update()" hint="Enter для сохранения")
 
