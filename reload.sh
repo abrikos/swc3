61de5bb3515c91f0513840633710a9e5d3ecad39
Artem Filippow <a.filippov@qtech.ru>
2025-09-13 15:58:42 +0900

    fix reload script

--- reload.sh
+++ reload.sh
@@ -4,6 +4,7 @@ GIT=`git pull`
 if [[ $GIT =~ "актуально." ]]; then
   echo $GIT
 else
+  echo 1
 ## Load nvm
 #  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 #  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
