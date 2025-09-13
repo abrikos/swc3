164b64f09000ab19eb584e546dbd1eace22d9045
Artem Filippow <a.filippov@qtech.ru>
2025-09-13 15:56:00 +0900

    fix reload script

--- reload.sh
+++ reload.sh
@@ -4,13 +4,13 @@ GIT=`git pull`
 if [[ $GIT =~ "актуально." ]]; then
   echo $GIT
 else
-# Load nvm
-  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm use 22.16.0
-  npm i
-  npm run build
-  #~/.nvm/versions/node/v22.16.0/bin/pm2 restart all
-  pm2 restart all
-  echo date > ~/swc3.updated.txt
+## Load nvm
+#  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+#  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+#  nvm use 22.16.0
+#  npm i
+#  npm run build
+#  #~/.nvm/versions/node/v22.16.0/bin/pm2 restart all
+#  pm2 restart all
+#  echo date > ~/swc3.updated.txt
 fi
